16e024d67c7fb1fbbc2ee39f5f43916182c42ae7
mromanella <matthew.romanella@gmail.com>
2019-05-20 23:29:01 -0400

    Fix log err

--- slitherIOAutoPilot.js
+++ slitherIOAutoPilot.js
@@ -88,7 +88,7 @@ class SlitherIOAutoPilot {
 
         if (this.foodCounts[foodID] > this.foodIdCountLimit) {
             delete this.foodCounts[foodID];
-            log('spinning wheels');
+            this.log('spinning wheels');
             this.boost(this.boostDuration);
         }
     }
